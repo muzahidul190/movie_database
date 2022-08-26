7d215591b062037e831214a3021dea0bb7f903ab
Muzahidul Islam <muzahidul190@gmail.com>
2022-08-26 20:47:00 +0600

    addata head fixed

--- addata/index.php
+++ addata/index.php
@@ -1,5 +1,6 @@
 <?php
-include '../head.php';
+$title = "Insert Data | ";
+include 'head.php';
 ?>
 
 
@@ -8,7 +9,12 @@ include '../head.php';
 include 'header.php';
 ?>
 
-
+<div class="btn-group" role="group" aria-label="Basic mixed styles example">
+    <a href="addactor.php" type="button" class="btn btn-danger">Add Actor</a>
+    <a href="addmovie.php" type="button" class="btn btn-warning">Add Movie</a>
+    <a href="addirector.php" type="button" class="btn btn-success">Add Director</a>
+    <a href="addgenre.php" type="button" class="btn btn-info">Add Genre</a>
+</div>
 
 <!--End Editing here-->
 
