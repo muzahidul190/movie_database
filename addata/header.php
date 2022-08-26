885d791a47002424bc29b99627c40f55ae3537e8
Muzahidul Islam <muzahidul190@gmail.com>
2022-08-26 20:25:06 +0600

    addata folder formation

--- addata/header.php
+++ addata/header.php
@@ -1,32 +1,32 @@
 <header class="row col-12">
     <nav class="navbar navbar-expand-lg bg-light navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img src="images/icon.ico" alt="" srcset=""></a>
+            <a class="navbar-brand" href="../index.php"><img src="images/icon.ico" alt="" srcset=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="Actors">Actors</a>
+                        <a class="nav-link" href="../actors.php">Actors</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Others
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="directors.php">Directors</a></li>
-                            <li><a class="dropdown-item" href="genres.php">Genres</a></li>
+                            <li><a class="dropdown-item" href="../directors.php">Directors</a></li>
+                            <li><a class="dropdown-item" href="../genres.php">Genres</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                        <a class="nav-link" href="../about.php">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="addata/index.php">Insert</a>
+                        <a class="nav-link" href="index.php">Insert</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
